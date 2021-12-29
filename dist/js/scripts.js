d292ecaba4f724813cf9058295cbe2537f91b21c
Ivan <ivan.frontcoder@gmail.com>
2021-12-29 20:29:09 +0500

    .

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -3,7 +3,7 @@
 //  Email: ivan.frontcoder@gmail.com
 
 document.addEventListener('DOMContentLoaded', () => {
-	
+
 	// Брэйкпоинты js
 	let	breakXl = 1400,
 			breakLg = 1200,
@@ -11,6 +11,39 @@ document.addEventListener('DOMContentLoaded', () => {
 			breakSm = 769,
 			breakXs = 500;
 	
+	function drop() {
+		let triggers = document.querySelectorAll('.js-dropTrigger');
+		for (let i = 0; i < triggers.length; i++) {
+			let trigger = triggers[i];
+			trigger.addEventListener('click', () => {
+				let drop = document.getElementById(trigger.getAttribute('data-drop'));
+				if (!trigger.classList.contains('active')) {
+					trigger.classList.add('active');
+					drop.classList.add('open');
+				}else {
+					trigger.classList.remove('active');
+					drop.classList.remove('open');
+				}
+				document.addEventListener('click', (e) => {
+					if (!trigger.contains(e.target)
+						&& !drop.contains(e.target)) {
+						trigger.classList.remove('active');
+						drop.classList.remove('open');
+					}
+				});
+			})
+		}
+	}
+	drop();
 	
+	const swiperWelcome = new Swiper('#welcomeSlider', {
+		// Optional parameters
+		loop: true,
+		// Navigation arrows
+		navigation: {
+			prevEl: '.welcome__button--prev',
+			nextEl: '.welcome__button--next',
+		},
+	});
 
 });
