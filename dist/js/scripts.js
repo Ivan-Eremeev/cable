08ab1278ed6821c86f536d4958f29fc1e37d70e7
Ivan <ivan.frontcoder@gmail.com>
2022-01-02 22:04:45 +0500

    .

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -5,15 +5,16 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Брэйкпоинты js
-	let	breakXl = 1400,
-			breakLg = 1200,
-			breakMd = 1025,
-			breakSm = 769,
-			breakXs = 500;
+	var breakXl = 1400,
+		breakLg = 1200,
+		breakMd = 1025,
+		breakSm = 769,
+		breakXs = 500;
 	
 	// Выпадайка при клике
 	function drop() {
-		let triggers = document.querySelectorAll('.js-dropTrigger');
+		let triggers = document.querySelectorAll('.js-dropTrigger'),
+				closes = document.querySelectorAll('.js-dropClose');
 		for (let i = 0; i < triggers.length; i++) {
 			let trigger = triggers[i];
 			trigger.addEventListener('click', () => {
@@ -32,6 +33,15 @@ document.addEventListener('DOMContentLoaded', () => {
 						drop.classList.remove('open');
 					}
 				});
+			})
+		}
+		for (let i = 0; i < closes.length; i++) {
+			let close = closes[i];
+			close.addEventListener('click', () => {
+				let drop = document.getElementById(close.getAttribute('data-drop')),
+						trigger = document.querySelector('[data-drop=' + close.getAttribute('data-drop') + ']');
+				drop.classList.remove('open');
+				trigger.classList.remove('active');
 			})
 		}
 	}
@@ -110,5 +120,68 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 	swiperToggleInit();
+
+	// Меню
+	function menu() {
+		let btn = document.getElementById('menu-btn'),
+				menu = document.getElementById('menu'),
+				close = document.getElementById('menu-close'),
+				childItems = document.querySelectorAll('.menu__child'),
+				submenus = document.querySelectorAll('.menu__drop'),
+				backs = document.querySelectorAll('.menu__back');
+		btn.addEventListener('click', () => {
+			if (!btn.classList.contains('active')) {
+				btn.classList.add('active');
+				menu.classList.add('open');	
+			}else {
+				btn.classList.remove('active');
+				menu.classList.remove('open');
+				for (let i = 0; i < childItems.length; i++) {
+					childItems[i].classList.remove('active');
+				}
+				for (let i = 0; i < submenus.length; i++) {
+					submenus[i].classList.remove('open');
+				}
+			}
+			document.addEventListener('click', (e) => {
+				if (!btn.contains(e.target)
+					&& !menu.contains(e.target)) {
+					btn.classList.remove('active');
+					menu.classList.remove('open');
+					for (let i = 0; i < childItems.length; i++) {
+						childItems[i].classList.remove('active');
+					}
+					for (let i = 0; i < submenus.length; i++) {
+						submenus[i].classList.remove('open');
+					}
+				}
+			});
+		})
+		close.addEventListener('click', () => {
+			btn.classList.remove('active');
+			menu.classList.remove('open');
+		})
+		for (let i = 0; i < childItems.length; i++) {
+			let childItem = childItems[i];
+			childItem.querySelector('a').addEventListener('click', (e) => {
+				e.preventDefault();
+				let submenu = childItem.querySelector('.menu__drop');
+				childItem.classList.add('active');
+				submenu.classList.add('open');
+				close.addEventListener('click', () => {
+					childItem.classList.remove('active');
+					submenu.classList.remove('open');
+				})
+			})
+		}
+		for (let i = 0; i < backs.length; i++) {
+			let back = backs[i];
+			back.addEventListener('click', () => {
+				back.closest('.menu__child').classList.remove('active');
+				back.closest('.menu__drop').classList.remove('open');
+			})
+		}
+	}
+	menu();
 
 });
